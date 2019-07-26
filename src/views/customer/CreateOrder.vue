43a4368ae141412ffaa0c87edc5b570ead254c1e
VeuraGJ <yuanxiao_ru@163.com>
2019-07-26 22:11:16 +0800

    update customer createOrder.vue
    修改label宽度和将邮箱输入框换成输入位置

--- src/views/customer/CreateOrder.vue
+++ src/views/customer/CreateOrder.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div>
-      <group>
+      <group label-width="5em">
         <x-input title="姓名" name="username" placeholder="请输入姓名" is-type="china-name"></x-input>
       </group>
 
-      <group >
+      <group label-width="5em">
         <x-input title="手机号码" name="mobile" placeholder="请输入手机号码" keyboard="number" is-type="china-mobile"></x-input>
       </group>
 
-      <group >
-        <x-input title="邮箱" name="email" placeholder="请输入邮箱地址" is-type="email"></x-input>
+      <group label-width="5em">
+        <x-input title="位置" name="position" placeholder="请输入位置信息" is-type="position"></x-input>
       </group>
 
       <x-button style="margin-top: 30px; width: 50%" plain type="primary">创建订单</x-button>
